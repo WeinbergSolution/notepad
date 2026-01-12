b2bd232c632291fac328088456c36947c8320d4e
WeinbergSolution <pascalgiercke@gmail.com>
2026-01-12 14:31:53 +0100

    add: fuction addNotice

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 // W Fragen, was, wann, wie, wo
 // notizen anzeigen lassen
 // ich brauche notizen
-let notes = ["Banana", "Rasenmähen", "Geld", "Müll"];
+let notes = [];
 
 // ich muss definieren wo sie anzuzeigen sind
 function randerNotes() {
-  let contenRef = document.getElementById("content");
-
+  let contenRef = document.getElementById("noticeContent");
+  contenRef.innerHTML = "";
   for (let indexNote = 0; indexNote < notes.length; indexNote++) {
     const note = notes[indexNote];
-    contenRef.innerHTML = " " + note;
+    contenRef.innerHTML += getNotesTamplate(note);
   }
 }
-// -> wann werden sie angezeigt
+
+function getNotesTamplate(note) {
+  return ` <p>+ ${note}</p>`;
+}
+
 // notizen hinzufügen
+//  eingabe auslesen
+function addNote() {
+  let noteInputRef = document.getElementById("note_input");
+  let noteInput = noteInputRef.value;
+  // eingabe den notizen hinzufügen
+  notes.push(noteInput);
+  // eingabe anzeigen lassen
+  randerNotes();
+  // clear Inputfield
+  noteInputRef.value = "";
+}
+
 // notizen löschen
 // notizen archivieren
